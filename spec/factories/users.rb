03fe183df89c593cca7a376494294cae7379a3a7
kei0316 <log3zozosan@icloud.com>
2025-02-28 18:18:55 +0900

    ユーザー管理機能

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
-  factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-  end
-end
+  require 'faker'
+
+  FactoryBot.define do
+    factory :user do
+      nickname              { Faker::Name.initials(number: 2) }
+      email                 { Faker::Internet.email }
+      password              { Faker::Internet.password(min_length: 6) }
+      password_confirmation { password }
+    end  # この行でファクトリブロックを閉じます
+  end  # この行で FactoryBot ブロックを閉じます
+end  
