ef838ac434f6631144c31b335bbacd756c2fd837
kei0316 <log3zozosan@icloud.com>
2025-04-24 19:12:55 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 require 'sprockets/railtie'
+require 'zeitwerk'
+require 'bootsnap/setup'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
